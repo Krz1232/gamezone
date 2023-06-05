a16006e335cbf4e786af3c9f8de7f42af7badd88
Diyari Oct <diyarimohammed33@gmail.com>
2023-06-05 22:00:23 +0300

    Source Updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
      <div className='foot_container'>
         <h4>All Rights Reserved © 2023 Diyari Mohammed</h4>
             <div className='footer-icons'>
-                <a href='https://github.com/Krz1232'><i className="fa-brands fa-github" style={{color : 'black'}}></i></a>
+                <a href='https://github.com/diyarim123'><i className="fa-brands fa-github" style={{color : 'black'}}></i></a>
                 <a href='https://www.facebook.com/diyari.mhamad.54'><i className="fa-brands fa-facebook" style={{color : 'black'}}></i></a>
                 <a href='https://www.instagram.com/diyarimathers/?hl=en'><i className="fa-brands fa-instagram" style={{color : 'black'}}></i></a>
                 <a href='https://www.linkedin.com/in/diyari-mohammed-3a2122228/'><i className="fa-brands fa-linkedin" style={{color : 'black'}}></i></a>
